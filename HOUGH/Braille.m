1ba0d317bfead4aab71a1303e4da2f7f728d50b4
lucanchling <luc.anchling@cpe.fr>
2022-06-02 18:06:21 +0200

    OK

--- HOUGH/Braille.m
+++ HOUGH/Braille.m
@@ -5,15 +5,76 @@ close all;
 clc;
 
 I = im2double(imread("./Braille/braille1.png"));
-I= I+0.2;
-
-
 
 % Filtrage --> Prétraitement
-% SE = strel('disk',5);   % Element structurant
-% I = imdilate(I,SE);
-% imshow(I)
 
+% Dilatation
+SE = strel('disk',3);   % Element structurant
+I = imdilate(I,SE);
+
+% TopHat
+S = strel('disk',37);   % Element structurant
+Ifiltered = imtophat(I,S);
+
+figure()
+imshow(Ifiltered)
+
+% Détection de cercles
+[centers, radius] = imfindcircles(Ifiltered,[2 20],'ObjectPolarity','bright');
+figure()
+imshow(I,[])
+viscircles(centers, radius,'Color','b')
+
+%% Braille - Image 2
+clear variables;
+close all;
+clc;
+
+I = im2double(imread("./Braille/braille2.png"));
+I = rgb2gray(I);
+% Filtrage --> Prétraitement
+
+
+% BottomHat
+S = strel('disk',10);   % Element structurant
+Ifiltered = imbothat(I,S);
+%figure()
+%imshow(Ifiltered,[])
+
+
+
+% Filtre médian
+Ifiltered = medfilt2(Ifiltered,[2 3]);
+
+% figure()
+% imshow(Ifiltered,[])
+
+SE = strel('disk',2);   % Element structurant
+Ifiltered = imdilate(Ifiltered,SE);
+figure()
+imshow(Ifiltered)
+
+% SE = strel('disk',4);   % Element structurant
+% Ifiltered = imdilate(Ifiltered,SE);
+
+% Détection de cercles
+[centers, radius] = imfindcircles(Ifiltered,[1 6]);
+figure()
+imshow(I,[])
+viscircles(centers, radius,'Color','b')
+
+
+%% Braille - Image 4
+clear variables;
+close all;
+clc;
+
+I = im2double(imread("./Braille/braille4.png"));
+
+% Filtrage --> Prétraitement
+SE = strel('disk',3);   % Element structurant
+I = imdilate(I,SE);
+% imshow(I)
 
 
 S = strel('disk',37);   % Element structurant
@@ -25,13 +86,14 @@ imshow(Ifiltered,[])
 % Ifiltered = imdilate(Ifiltered,SE);
 
 % Détection de cercles
-[centers, radius] = imfindcircles(Ifiltered,[1 15],'ObjectPolarity','bright');
+[centers, radius] = imfindcircles(Ifiltered,[2 20],'ObjectPolarity','bright');
 figure()
 imshow(I,[])
 viscircles(centers, radius,'Color','b')
 
-
 %%
+% S = strel('disk',3);   % Element structurant
+% Ifiltered = imopen(Ifiltered,S);
 % Egalisation d'histograme
 Ifiltered = histeq(I);
 % figure()
